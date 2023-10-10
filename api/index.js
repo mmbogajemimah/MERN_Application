085be6965fc3bb7db7438b957ba721ced5c04f49
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-10-10 21:56:11 +0300

    Backend Application in Node.js

---
+++ api/index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const bcrypt = require('bcryptjs')
+const User = require('./models/User.js')
+require('dotenv').config()
+const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
+
+// Parses JSON from the Request
+// Password: Q7uuCFXnVX4PxjHe
+app.use(express.json())
+
+app.use(cors({
+    credentials: true,
+    origin: 'http://127.0.0.1:5173',
+}));
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('MongoDB connection error:', error);
+  });
+
+
+// mongoose.connect(process.env.MONGO_URL)
+
+console.log(process.env.MONGO_URL)
+
+app.get('/test', (req, res) => {
+    res.json('test ok');
+});
+
+app.post('/register', async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+  
+      const userDoc = await User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcryptSalt),
+      });
+  
+      res.json(userDoc);
+    } catch (error) {
+      console.error(error);
+      res.status(422).json({ error: 'unprocessable entity' });
+    }
+  });
+  
+  
+
+app.listen(4000);
