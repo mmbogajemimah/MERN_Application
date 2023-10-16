240ba7dd605bba715527a2402b8c924a96e550c0
Jemimah Mmboga <mmbogajemimah@gmail.com>
2023-10-16 07:13:16 +0300

    logout

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -1,10 +1,15 @@
 import { useContext } from "react"
 import { UserContext } from "../UserContext"
 import { Link, Navigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function AccountPage() {
     const { ready,user } = useContext(UserContext);
-    const { subpage } = useParams();
+    let { subpage } = useParams();
+
+    async function logout() {
+        await axios.post('/logout');
+    }
 
     if(!ready){
         return 'Loading...';
@@ -14,23 +19,33 @@ export default function AccountPage() {
         return <Navigate to={'/login'} />
     }
 
+    if (subpage === undefined) {
+        subpage = 'profile'
+    }
     console.log(subpage);
 
     function linkClasses (type=null) {
         let classes = 'py-2 px-6';
-        if (type === subpage || (subpage === undefined && type === 'profile')) {
+        if (type === subpage) {
             classes += " bg-primary text-white rounded-full";
         }
         return classes
     }
 
 
-
     return (
-        <div className="w-full flex justify-center mt-8 gap-2">
-            <Link className={linkClasses('profile')} to={'/account'}>My Profile</Link>
-            <Link className={linkClasses('bookings')} to={'/account/bookings'}>My Bookings</Link>
-            <Link className={linkClasses('places')} to={'/account/places'}>My Places</Link>
+        <div>
+            <nav className="w-full flex justify-center mt-8 gap-2 mb-2">
+                <Link className={linkClasses('profile')} to={'/account'}>My Profile</Link>
+                <Link className={linkClasses('bookings')} to={'/account/bookings'}>My Bookings</Link>
+                <Link className={linkClasses('places')} to={'/account/places'}>My Places</Link>
+            </nav>
+            {subpage === 'profile' && (
+                <div className="text-center max-w-lg mx-auto">
+                    Logged in as {user.name} ({user.email}) <br />
+                    <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
+                </div>
+            )}
         </div>
     )
 }
